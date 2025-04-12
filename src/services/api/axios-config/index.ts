0d55d090f436a99fe3b06814dff67d622855a2b9
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2025-04-12 18:35:47 -0300

    refactor(api): update base URL for requests

--- src/services/api/axios-config/index.ts
+++ src/services/api/axios-config/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiFetch = axios.create({
-	baseURL: `${process.env.REACT_APP_URL}/api/v2`,
+	baseURL: `${process.env.REACT_APP_URL}/v2`,
 	headers: {
 		'Content-Type': 'application/json',
 	},
