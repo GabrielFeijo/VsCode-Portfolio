44a7f750480993dc3307211966e637ee6ccd6f62
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2025-12-13 20:36:33 -0300

    refactor: remove commented-out rollupOptions and chunkSizeWarningLimit in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: 'build',
-        // rollupOptions: {
-        //     output: {
-        //         manualChunks(id) {
-        //             if (id.includes('node_modules')) {
-        //                 return id.toString().split('node_modules/')[1].split('/')[0].toString();
-        //             }
-        //         },
-        //     },
-        // },
-        // chunkSizeWarningLimit: 1000, // Adjust the chunk size limit (in kB)
+        outDir: 'build'
     },
     server: {
         host: "localhost",
